3160d22a7e50b1b35bb9cd06e317a99d1a3d9052
WhaleToaster <sollkre@gmail.com>
2025-05-19 22:05:04 +0200

    fastapi and index.html

--- app/main.py
+++
@@ -1,8 +0,0 @@
-import requests
-import os
-from pathlib import Path
-from dotenv import load_dotenv
-
-env_path = Path("../secrets/.env")
-load_dotenv(dotenv_path=env_path)
-api_key = os.getenv("GROQ_API")
