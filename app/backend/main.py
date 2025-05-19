dd369f8b3d09d183e9543f72f98f80b7534d38d1
WhaleToaster <sollkre@gmail.com>
2025-05-19 22:28:05 +0200

    adjusted llm variables

--- app/backend/main.py
+++ app/backend/main.py
@@ -5,7 +5,16 @@ from dotenv import load_dotenv
 from fastapi import FastAPI 
 from pydantic import BaseModel 
 
+from fastapi.middleware.cors import CORSMiddleware
+
+
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # You can restrict this later
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 env_path = Path("../secrets/.env")
 load_dotenv(dotenv_path=env_path)
@@ -27,10 +36,10 @@ def generate_anki_cards(data: PromptRequest):
         ],
         "mode": "instruct",
         "character": "Assistant",
-        "max_tokens": 800,
-        "top_p": 0.9,
-        "temperature": 1,
-        "seed": 10,
+        "max_tokens": 4092,
+        "top_p": 0.95,
+        "temperature": 1.2,
+        "seed": -1,
 
     }
 
