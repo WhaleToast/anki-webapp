29d66d3e8d2d81e47bad2f6018d5a5d6b77228fb
WhaleToast <sollkre@gmail.com>
2025-05-20 13:30:54 +0200

    together.ai and pyrightconfig

---
+++ app/backend/togai.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+from together import Together
+from dotenv import load_dotenv 
+import os
+
+envPath = Path("../secrets/.env")
+load_dotenv(dotenv_path=envPath)
+apiKey = os.getenv("togetherAPI")
+
+client = Together(api_key=apiKey)
+stream = client.chat.completions.create(
+    model="meta-llama/Llama-3.3-70B-Instruct-Turbo-Free",
+    messages=[{"role": "user", "content": "Hello"}],
+    stream=True,
+)
+
+for chunk in stream:
+    print(chunk.choices[0].delta.content or "", end="", flush=True)
